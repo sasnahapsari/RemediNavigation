c0dcb72be9f29d27b578669a76ad1a06abc2386b
sasnahapsari <sasnahapsari@gmail.com>
2017-06-07 13:15:50 +0700

    end

---
+++ app/src/main/java/xir5n29/remidiuts/telkomschid/navigationunited/MainActivity.java
@@ -0,0 +1,46 @@
+package xir5n29.remidiuts.telkomschid.navigationunited;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Navigation");
+
+        Button bt1 = (Button) findViewById(R.id.button1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OneActivity.class));
+            }
+        });
+        Button bt2 = (Button) findViewById(R.id.button2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TwoActivity.class));
+            }
+        });
+        Button bt3 = (Button) findViewById(R.id.button3);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ThreeActivity.class));
+            }
+        });
+        Button bt4 = (Button) findViewById(R.id.button4);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FourActivity.class));
+            }
+        });
+    }
+}
